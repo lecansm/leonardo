edc37260c9f956c6a5a38749d28f6b1356278e75
LeonardoDaRocha745 <61739825+LeonardoDaRocha745@users.noreply.github.com>
2022-08-29 23:11:12 -0300

    Add files via upload

---
+++ js/Main.js
@@ -0,0 +1,19 @@
+function TrocaSkill() {
+  document.getElementById("corpo").style.display="block";
+  document.getElementById("corpo2").style.display="none";
+  
+}
+  
+  function TrocaSkill2() {
+    document.getElementById("corpo").style.display="none";
+  document.getElementById("corpo2").style.display="block";
+
+
+    }
+
+  function changeDivContent(btn) {
+      content.innerHTML = btn.value
+    }
+
+
+    
