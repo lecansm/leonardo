11d3fc0deea1efc4415ff4efab0833e4e6d465b9
LeonardoDaRocha745 <leonardodaredevil@gmail.com>
2023-02-22 17:52:34 -0300

    Escondendo navbar

--- js/Main.js
+++ js/Main.js
@@ -20,3 +20,13 @@ function TrocaSkill2() {
 function changeDivContent(btn) {
   content.innerHTML = btn.value
 }
+var eixoAnterior = window.pageYOffset;
+window.onscroll = function() {
+  var atualEixo = window.pageYOffset;
+  if (eixoAnterior > atualEixo) {
+    document.getElementById("BarraNav").style.top = "0";
+  } else {
+    document.getElementById("BarraNav").style.top = "-100px";
+  }
+  eixoAnterior = atualEixo;
+}
