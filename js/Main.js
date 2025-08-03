b9bcb7b7690c8792fa3d91d075f6cae3ef36d496
LeonardoDaRocha745 <leonardodaredevil@gmail.com>
2025-08-03 06:03:20 -0300

    Mudando pra versão 2.0

--- js/Main.js
+++ js/Main.js
@@ -2,26 +2,6 @@ window.addEventListener('load', AOS.refresh)
 AOS.init();
 
 
-function TrocaSkill() {
-  document.getElementById("corpo").style.display = "block";
-  document.getElementById("corpo2").style.display = "none";
-  document.getElementById("botaosoft").style.backgroundImage = "linear-gradient(to right, #212121, #1d1d1d, #181818, #141414, #0f0e0e)";
-  document.getElementById("botaohard").style.backgroundImage = "linear-gradient(to right, #000000, #2a0d13, #4d0b17, #6f0712, #8e0e00)";
-  document.getElementById("botaohard").style.border = "2px solid #000";
-}
-
-function TrocaSkill2() {
-  document.getElementById("corpo").style.display = "none";
-  document.getElementById("corpo2").style.display = "block";
-  document.getElementById("botaosoft").style.backgroundImage = "linear-gradient(to right, #000428, #031a40, #062a5a, #073b75, #004e92)";
-  document.getElementById("botaohard").style.backgroundImage = "linear-gradient(to right, #212121, #1d1d1d, #181818, #141414, #0f0e0e)";
-  document.getElementById("botaosoft").style.border = "2px solid #000";
-}
-
-function changeDivContent(btn) {
-  content.innerHTML = btn.value
-}
-
 if (navigator.userAgent.includes("Instagram")) {
   document.getElementById("TituloNav").style.fontSize = "16px";
   document.getElementById("imgLogo").style.width = "40px";
@@ -31,6 +11,85 @@ if (navigator.userAgent.includes("Instagram")) {
   document.getElementById("cardHabilidades2").style.height = "280px !important";
 }
 
+ document.addEventListener('DOMContentLoaded', function() {
+    const sonic = document.getElementById('sonic');
+    const ring = document.getElementById('ring');
+    const ring2 = document.getElementById('ring2');
+    const ring3 = document.getElementById('ring3');
+    const html = document.getElementById('html');
+    const css = document.getElementById('css');
+    const js = document.getElementById('js');
+    const brilho = document.getElementById('brilho');
+    const brilho2 = document.getElementById('brilho2');
+    const brilho3 = document.getElementById('brilho3');
+    
+    ring.addEventListener('click', function() {
+      sonic.classList.add('sonic-run');
+      sonic.src = 'img/sonic4.gif';
 
+      html.classList.add('girar-trocar');
+      setTimeout(function() {
+        html.src = 'img/linguagens/programing.png'; 
+        html.classList.add('invertida');
+      }, 500);
+
+      html.addEventListener('animationend', function trocarImg() {
+        html.classList.remove('girar-trocar');
+        html.removeEventListener('animationend', trocarImg);
+        ring.style.display = 'none';
+        brilho.style.display = 'block';
+        setTimeout(function() {
+        brilho.style.display = 'none';
+      }, 300);
+      });
+    });
+    sonic.addEventListener('animationend', function() {
+      sonic.src = 'img/sonic3.gif';
+      ring
+    });
+    
+    ring2.addEventListener('click', function() {
+      sonic.classList.add('sonic-run2');
+      sonic.src = 'img/sonic4.gif';
+
+      css.classList.add('girar-trocar');
+      setTimeout(function() {
+      css.src = 'img/linguagens/react.png'; 
+      }, 500);
+
+      css.addEventListener('animationend', function trocarImg() {
+        css.classList.remove('girar-trocar');
+        css.removeEventListener('animationend', trocarImg);
+        css.classList.add('invertida'); 
+        ring2.style.display = 'none';
+        brilho2.style.display = 'block';
+        setTimeout(function() {
+        brilho2.style.display = 'none';
+      }, 300);
+        });
+    });
+
+    ring3.addEventListener('click', function() {
+      sonic.classList.add('sonic-run3');
+      sonic.src = 'img/sonic4.gif';
+
+      js.classList.add('girar-trocar');
+      setTimeout(function() {
+      js.src = 'img/linguagens/brands.png'; 
+      }, 500);
+
+      js.addEventListener('animationend', function trocarImg() {
+        js.classList.remove('girar-trocar');
+        js.removeEventListener('animationend', trocarImg);
+        js.classList.add('invertida');
+        ring3.style.display = 'none'; 
+        brilho3.style.display = 'block';
+        setTimeout(function() {
+        brilho3.style.display = 'none';
+      }, 300);
+        });
+    });
+  
+});
 
 
